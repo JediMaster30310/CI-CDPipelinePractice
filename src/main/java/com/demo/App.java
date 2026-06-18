a27ea5c6dadb7480651fb13c57bf5be0435a5ca1
Theo <303theob@gmail.com>
2026-06-18 16:00:25 +0100

    added multiply function and unit test fix

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -9,7 +9,7 @@ public class App {
     }
 
     public static int multiply(int a, int b) {
-        return a + b;
+        return a * b;
     }
     public static int square(int x) {
         return x * x;
